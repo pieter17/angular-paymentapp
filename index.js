99c0a2824ae43d5556dcd2f7701a2bd9d3fb71b2
Pieter Wenji <pieterjake321@gmail.com>
2021-12-14 16:28:39 +0700

    config for deploy

---
+++ index.js
@@ -0,0 +1,22 @@
+function requireHTTPS(req, res, next) {
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
+const express = require("express");
+const app = express();
+
+app.use(requireHTTPS);
+app.use(express.static("./dist/finalProject"));
+
+app.get("/*", (req, res) => {
+  res.sendFile("index.html", { root: "dist/finalProject" });
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`App is ready at http://localhost:${port}`);
+});
